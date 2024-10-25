5b71e1eb5d84fdf0fdeab9b541c9a82f2bb31499
Ankan Ghosh <ankan.ghosh@datacoresystems.co.in>
2024-10-25 09:48:16 +0530

    nifty weekly option

--- expiary_strike_data.js
+++ expiary_strike_data.js
@@ -1,6 +1,9 @@
 module.exports = {
-  "expiryDate": "24-04-2024",
-  "upstoxurl": "https://service.upstox.com/option-analytics-tool/open/v1/strategy-chains?assetKey=NSE_INDEX%7CNifty+Bank&strategyChainType=PC_CHAIN&expiry=",
-  "callKey": "48900.0",
-  "putKey": "46500.0"
+  "expiryDate": "31-10-2024",
+  "upstoxurl": "https://service.upstox.com/option-analytics-tool/open/v1/strategy-chains?assetKey=NSE_INDEX%7CNifty+50&strategyChainType=PC_CHAIN&expiry=",
+  "callKey": "24000.0",
+  "putKey": "24000.0"
 };
+
+// https://service.upstox.com/option-analytics-tool/open/v1/strategy-chains?assetKey=NSE_INDEX%7CNifty+Bank&strategyChainType=PC_CHAIN&expiry=
+// https://service.upstox.com/option-analytics-tool/open/v1/strategy-chains?assetKey=NSE_INDEX%7CNifty+50&strategyChainType=PC_CHAIN&expiry=
